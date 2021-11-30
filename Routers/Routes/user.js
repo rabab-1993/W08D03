4227535dae9112449da436f4dfc50dd23ec188ae
rabab-1993 <rabab.abdulaziz.elq.js@tuwaiq.edu.sa>
2021-11-30 20:43:08 +0300

    create authorization file

--- Routers/Routes/user.js
+++ Routers/Routes/user.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const userRouter = express.Router();
 
-const {register, logIn, allUser} = require('../Controllers/user')
+const {register, logIn, allUser} = require('../Controllers/user');
+const authentication = require('../midleware/auth')
+const authorization = require('../midleware/outh')
 
-
-userRouter.get("/", allUser);
 userRouter.post("/register", register);
 userRouter.post("/login", logIn);
 
+// just for admin
+userRouter.get("/",authentication, authorization, allUser);
+userRouter.delete("/",authentication, authorization, allUser);
 
 
 
